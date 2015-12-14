e18e2e1e075a30cd16b40dd7da40a46314dbb50a
Xavier Tô <xavier.to@shopify.com>
2015-12-14 07:44:03 -0500

    Json response works, now to accumulate that shiieeet

--- elm-tempchart.elm
+++ elm-tempchart.elm
@@ -1,56 +1,75 @@
 import Http
-
 import Json.Decode as Json exposing ((:=))
-
 import Task exposing (..)
-
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import String exposing (..)
 
-import String
 
--- View
+
 
 view : String -> Result String (List String) -> Html
 view string result =
-  let message = 
-    case result of 
-      Err msg ->
-        [div [] [ text "ERROR" ]]
-      Ok temp ->
-        [span [] [text temp]]
-
+  let queryInput =
+        input
+          [placeholder "enter whatever"
+          , value string
+          , on "input" targetValue (Signal.message query.address)
+          , myStyle
+          ]
+          []
+      tempSpan =
+        case result of
+          Err msg ->
+            [ div [myStyle] [text msg]]
+          Ok temps ->
+            List.map (\temp -> span [myStyle][text temp]) temps
   in
-    div [] (message)
+    div[] (queryInput :: tempSpan)
 
--- WIRING
 
+myStyle : Attribute
+myStyle =
+  style
+      [ ("width", "100%")
+      , ("height", "40px")
+      , ("padding", "10px 0")
+      , ("font-size", "2em")
+      , ("text-align", "center")
+      ]
+
+--Wiring
+main : Signal Html
 main =
-  Signal.map2 view query.signal result.signal
+  Signal.map2 view query.signal results.signal
 
 query : Signal.Mailbox String
-query = 
+query =
   Signal.mailbox ""
 
-result : Signal.Mailbox (Result String (List String))
-result = 
-  Signal.mailbox (Err "I'm a tomato!")
+results: Signal.Mailbox (Result String (List String))
+results =
+  Signal.mailbox (Err "Potato")
 
-port request : Signal (Task x ())
-port request = 
+port updateContent : Signal (Task x ())
+port updateContent =
   Signal.map getTemp query.signal
-    |> Signal.map (\task -> Task.toResult task `andThen` Signal.send result.address)
+    |> Signal.map (\task -> Task.toResult task `andThen` Signal.send results.address)
 
-getTemp : Task String (List String)
-getTemp  = 
-  "http://localhost:3000/getTemp" `andThen` (mapError (always "BOOM !") << Http.get temp)
+getTemp : String -> Task String (List String)
+getTemp query =
+  let testUrl =
+      if String.length query /= 0
+        then succeed ("http://localhost:3000/" ++ query)
+        else fail "Matt Damon needs 1+ char"
+  in
+    testUrl `andThen` (mapError (always "BOOM") << Http.get jd)
 
-temp : Json.Decoder (List String)
-temp = 
-  let tempAtTime = 
-    Json.object2 (\temperature time -> temperature ++ ", " ++ time)
-      ("temperature reading" := Json.string)
-      ("time" := Json.string)
-  in 
-    "temperatureData" := Json.list tempAtTime
+jd : Json.Decoder (List String)
+jd =
+  let tempobj =
+    Json.object1 (\temperature-> temperature ++ "C")
+        ("temperature" := Json.string)
+  in
+    "temperatures" := Json.list tempobj
