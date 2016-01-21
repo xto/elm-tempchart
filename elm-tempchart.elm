61158719660be2551736509a9a656313157c80a0
Xavier Tô <xavier.to@shopify.com>
2016-01-21 07:36:22 -0500

    Posting data to server

--- elm-tempchart.elm
+++ elm-tempchart.elm
@@ -1,5 +1,6 @@
 import Http
 import Json.Decode as Json exposing ((:=))
+-- import Json.Encode
 import Task exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
@@ -16,12 +17,28 @@ type alias TemperatureReading =
   {temperature: Float, readAt: String, unit: String}
 
 
+type alias BasicHttpReponse =
+  {status: Int, body: String}
+
 type alias Model = {
   temperatureReadings: List TemperatureReading
   , mashName: String
   , mashNamed: Bool
 }
 
+temperatureReadingToJsonString: TemperatureReading -> String
+temperatureReadingToJsonString temperatureReading =
+  let
+    temperatureString =
+      "'temperature': '" ++ (toString temperatureReading.temperature) ++ "'"
+    comma =
+      ", "
+    readAtString =
+      "'readAt': '" ++temperatureReading.readAt++ "'"
+    unitString =
+      "'unit': '" ++temperatureReading.unit++ "'"
+  in
+    "{"++ temperatureString ++ comma ++ readAtString ++ comma ++ unitString ++"}"
 
 init : (Model, Effects Action)
 init =
@@ -33,10 +50,12 @@ init =
 
 type Action
   = LoadReadings (Maybe (List TemperatureReading))
+  | PostReadings
   | RequestReadings
   | RequestReadingsOnTime (Effects Action)
   | SetMashName
   | UpdateMashName String
+  | NoOp (Maybe BasicHttpReponse)
 
 
 update: Action -> Model -> (Model, Effects Action)
@@ -60,21 +79,37 @@ update action model =
             )
     RequestReadingsOnTime effect ->
       (model, effect)
+
     SetMashName ->
-      (Model model.temperatureReadings model.mashName True
-      ,Effects.none)
+      if model.mashName /= "" then
+        (Model model.temperatureReadings model.mashName True
+        ,Effects.none)
+      else
+        (model, Effects.none)
 
     UpdateMashName mashName ->
       (Model model.temperatureReadings mashName model.mashNamed
       ,Effects.none)
+
+    PostReadings ->
+      (model, (postReadings model))
+
+    NoOp response ->
+      case response of
+        Nothing ->
+          (model, Effects.none)
+        Maybe.Just garbage ->
+          (model, Effects.none)
+
 
 
 view: Signal.Address Action -> Model ->Html
 view address model =
 
   div[class "container"]
-  [ entryForm address model,
-    (drawChart (extractAllTimes model) (extractAllTemps model))
+  [ entryForm address model
+    ,(drawChart (extractAllTimes model) (extractAllTemps model))
+    ,saveButton address model
   ]
 
 extractAttributes : (a -> b) -> List a -> List b
@@ -127,8 +162,28 @@ entryForm address model =
       setMashNameButton
     ]
 
+saveButton: Signal.Address Action-> Model -> Html
+saveButton address model =
+  div [] [
+    button[ class "setName", onClick address PostReadings] [text "Save Data"]
+  ]
+
 
 --Wiring
+postReadings : Model -> Effects Action
+postReadings model =
+  let
+    potato =
+      Http.multipart
+        [ Http.stringData "mash_name" (model.mashName)
+        , Http.stringData "temperatures" (toString(List.map temperatureReadingToJsonString model.temperatureReadings))
+        ]
+  in
+    Http.post basicApiResponseJsonDecoder (Http.url "http://localhost:3000/temperatures" []) (potato)
+    |> Task.toMaybe
+    |> Task.map NoOp
+    |> Effects.task
+
 getTempAction : Effects Action
 getTempAction =
     getTempFromApiTask
@@ -138,12 +193,18 @@ getTempAction =
 
 getTempFromApiTask :Task a (Maybe (List TemperatureReading))
 getTempFromApiTask  =
-  Http.get jd (Http.url "http://localhost:3000/temperatures" [])
+  Http.get temperatureReadingsJsonDecoder (Http.url "http://localhost:3000/temperatures" [])
   |> Task.toMaybe
 
+basicApiResponseJsonDecoder : Json.Decoder BasicHttpReponse
+basicApiResponseJsonDecoder =
+  Json.object2
+    BasicHttpReponse
+    ("status" := Json.int)
+    ("body" := Json.string)
 
-jd : Json.Decoder (List TemperatureReading)
-jd =
+temperatureReadingsJsonDecoder : Json.Decoder (List TemperatureReading)
+temperatureReadingsJsonDecoder =
   let tempobj =
     Json.object3
         TemperatureReading
@@ -152,6 +213,8 @@ jd =
         ("unit" := Json.string)
   in
     "temperatures" := Json.list tempobj
+
+
 clock: Signal Time
 clock =
   Time.every (2 * Time.second)
